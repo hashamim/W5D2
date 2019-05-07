98bfc5fe297c3d5fbf5d14a8647e341dff1e366b
Hasnain <hashamim@gmail.com>
2019-05-07 11:23:10 -0400

    start reddit

---
+++ roddit/app/models/user.rb
@@ -0,0 +1,49 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#
+
+class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    before_initialize :ensure_session_token
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        pw_digest=BCrypt::Password.new(password_digest)
+        pw_digest.is_password?(password)
+    end
+
+    def self.find_by_credentials(username,password)
+        u = User.find_by(username: username)
+        if u.is_password?(password)
+            u 
+        else
+            nil
+        end
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = self.generate_session_token
+    end
+end
